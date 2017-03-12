46aa63a28f653944cd642f1f8a2a58d06fb00696
Phien Pham <phien@triip.me>
2017-03-12 09:05:05 +0700

    Implement Stochastic Gradient Descent

--- gradient_example.py
+++ gradient_example.py
@@ -8,7 +8,8 @@ Make your changes to backward method of the Sigmoid class in miniflow.py
 """
 
 import numpy as np
-from miniflow import *
+from miniflow_with_gd import *
+
 
 X, W, b = Input(), Input(), Input()
 y = Input()
--- miniflow_with_gd.py
+++ miniflow_with_gd.py
@@ -256,3 +256,17 @@ def forward_and_backward(graph):
     # see: https://docs.python.org/2.3/whatsnew/section-slices.html
     for n in graph[::-1]:
         n.backward()
+
+
+def sgd_update(trainables, learning_rate=1e-2):
+    """
+    Updates the value of each trainable with SGD.
+
+    Arguments:
+
+        `trainables`: A list of `Input` Nodes representing weights/biases.
+        `learning_rate`: The learning rate.
+    """
+    for input_node in trainables:
+        partial = input_node.gradients[input_node]
+        input_node.value -= learning_rate * partial
